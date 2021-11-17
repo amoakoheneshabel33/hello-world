b49c89bc384c0656c411599477e7197a99b7662e
amoakoheneshabel33 <samoakohene0022@st.ug.edu.gh>
2021-11-17 21:52:01 +0000

    First Commit

---
+++ app/Const/Hairs.js
@@ -0,0 +1,54 @@
+const Hairs = [
+    {
+      "id": 1,
+      "name": 'school bag',
+      "price": '39.99',
+      "like": true,
+      "img": require('../assets/sb.jpg'),
+      "about":'long'
+    },
+  
+    {
+      "id": 2,
+      "name": 'Orange wig',
+      "price": '29.99',
+      "like": false,
+      "img": require('../assets/sb.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 3,
+      "name": 'Long Black hair',
+      "price": '25.99',
+      "like": false,
+      "img": require('../assets/sb.jpg'),
+      "about":'long'
+    },
+  
+    {
+      "id": 4,
+      "name": 'ladies wig',
+      "price": '25.99',
+      "like": true,
+      "img": require('../assets/sb.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 5,
+      "name": 'Curly hair',
+      "price": '50.99',
+      "like": true,
+      "img": require('../assets/sb.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 6,
+      "name": 'white wig',
+      "price": '50.99',
+      "like": false,
+      "img": require('../assets/sb.jpg'),
+      'about':'long'
+    },
+  ];
+  
+  export default Hairs;
