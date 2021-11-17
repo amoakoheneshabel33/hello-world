60e84c84a0d57e5f43b88fe58d30eaf25c26a676
amoakoheneshabel33 <samoakohene0022@st.ug.edu.gh>
2021-11-17 23:01:52 +0000

    picture change

--- app/Const/Hairs.js
+++ app/Const/Hairs.js
@@ -10,43 +10,43 @@ const Hairs = [
   
     {
       "id": 2,
-      "name": 'Orange wig',
+      "name": 'Designer bag',
       "price": '29.99',
       "like": false,
-      "img": require('../assets/sb.jpg'),
+      "img": require('../assets/bag1.jpg'),
       "about":'long'
     },
     {
       "id": 3,
-      "name": 'Long Black hair',
+      "name": 'Designer Bag ',
       "price": '25.99',
       "like": false,
-      "img": require('../assets/sb.jpg'),
+      "img": require('../assets/bag2.jpg'),
       "about":'long'
     },
   
     {
       "id": 4,
-      "name": 'ladies wig',
+      "name": 'Designer bag',
       "price": '25.99',
       "like": true,
-      "img": require('../assets/sb.jpg'),
+      "img": require('../assets/bag3.jpg'),
       "about":'long'
     },
     {
       "id": 5,
-      "name": 'Curly hair',
+      "name": 'designer bag',
       "price": '50.99',
       "like": true,
-      "img": require('../assets/sb.jpg'),
+      "img": require('../assets/bag4.jpg'),
       "about":'long'
     },
     {
       "id": 6,
-      "name": 'white wig',
+      "name": 'Designer bag',
       "price": '50.99',
       "like": false,
-      "img": require('../assets/sb.jpg'),
+      "img": require('../assets/bag5.jpg'),
       'about':'long'
     },
   ];
